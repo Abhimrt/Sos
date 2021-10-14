81123e1d233986990c2ec40e24f05be4772f3d17
ABHISHEK <singhal@ABHISHEKs-MacBook-Air.local>
2021-10-14 19:23:06 +0530

    responsive nav bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+//  =======================navbar========================== //
+let nav =document.getElementById("navbar");
+let  opennav=document.getElementById("openNav");
+let  closenav=document.getElementById("closeNav");
+function openNav(){
+    nav.style.right="0"; 
+}
+function closeNav(){
+    nav.style.right="-50vw"; 
+}
+
+
+
+
+
+
+
+
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
