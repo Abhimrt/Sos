d2ce207208b692c3c40ab588422b51eab5e2e286
ABHISHEK <singhal@ABHISHEKs-MacBook-Air.local>
2021-10-19 19:21:09 +0530

    test v.3

--- js/test.js
+++ js/test.js
@@ -1,9 +1,45 @@
-// alert("This Page contain same Quiz related to HTMl. Each correct answer contain 1 marks.You have to score atleast 6 marks to pass the test")
+// ================to disable right click====================
+document.body.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+})
+// ===================time limit==============================
+let a = 60;
+let time = setInterval(timePeriod,1000);
+let timedisplay = document.getElementById('timeDisplay');
+function timePeriod(){
+    if(cont.style.top=="-105vh"){
+        if(a==0){
+            clearInterval(time);
+            check(); 
+        }
+        else{
+        a--;
+        timedisplay.innerHTML = a+' sec';}
+    }
+}
+// ================to scroll page at top====================
+window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+//====================to perform task on the button==========
 const cont = document.getElementById('testContainer');
 function closeContainer() {
     cont.style.top="-105vh";
+    document.body.style.overflow="auto";
 }
+// ===========to check the test marks =====================
+let res= document.getElementById('result');
 function check(){
+    //=======================dom makeover============
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    cont.style.top="0vh";
+    document.body.style.overflow="hidden";
+    document.getElementById('button').style.display="none";
+    //================to check no==========================
     let i=0;
     if(document.getElementById("Ques1").checked==true)
     i++;
@@ -26,7 +62,14 @@ function check(){
     if(document.getElementById("Ques10").checked==true)
     i++;
     if(i<6)
-    alert("Total marks out of 10 is = "+i+" 'Fail'");
+    res.innerHTML="Total marks out of 10 is = "+i+" 'fail'";
     else
-    alert("Total marks out of 10 is = "+i+" 'Pass'");
+    res.innerHTML="Total marks out of 10 is = "+i+" 'Pass'";
+    // ====================reload page=====================
+    let counter = 5;
+    setInterval(()=>{
+        counter--;
+        if(counter==0)
+        location.reload();
+    },1000)
 }
